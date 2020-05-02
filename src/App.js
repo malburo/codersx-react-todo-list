4a556c7c279ac5af65b7e39429e1dfe96e5eeb6d
malburo <tongquocbao0605@gmail.com>
2020-05-02 21:54:14 +0700

    base syntax

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const todos = [
+  'Go to market',
+  'Buy food',
+  'Make dinner'
+];
 
+class App extends React.Component {
+  render(){
+    let items = todos.map(item => {
+      return <li>{item}</li>
+    })
+    return (
+      <div className="App">
+        <ul>{items}</ul>
+      </div>
+    )
+  }
+}
 export default App;
